c91131fa771f8a8ce5a1e433fc62cb5eaad24316
Shruti <sdnk1993@gmail.com>
2022-09-06 14:15:22 +0530

    welcome

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+public class EmployeeWage {
+    public static void welcome(){
+        System.out.println("welcome to Employee wage computation");
+    }
+
+    public static void main(String[] args) {
+        EmployeeWage emp =new EmployeeWage();
+        emp.welcome();
+    }
+}
+

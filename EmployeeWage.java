a3847801ee08f271f93e6919107ded32cd063c23
Shruti <sdnk1993@gmail.com>
2022-09-06 14:17:24 +0530

    UC1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,16 @@
 public class EmployeeWage {
-    public static void welcome(){
-        System.out.println("welcome to Employee wage computation");
+    public static int randomval() {
+        int empcheck = (int) (Math.random() * 10 % 2);
+        return empcheck;
     }
-
     public static void main(String[] args) {
-        EmployeeWage emp =new EmployeeWage();
-        emp.welcome();
+        EmployeeWage emp = new EmployeeWage();
+        int chk = emp.randomval();
+        if (chk == 0) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
 
